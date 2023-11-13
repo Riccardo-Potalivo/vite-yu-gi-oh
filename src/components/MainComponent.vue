041e555b6075395803b295c68427654999d5597e
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-11-13 16:48:00 +0100

    main-set

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <main>
+        <div class="container">
+            <section class="p-3">
+                
+            </section>
+
+            <section class="card_container">
+                <div>
+                    <h6 class="m-0">Found</h6>
+                </div>
+
+            </section>
+        </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'MainComponent',
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as *;
+
+main {
+    background-color: $PrimaryColor;
+}
+
+.card_container {
+    background-color: $White;
+    padding: 3rem;
+    
+    div:first-child {
+        background-color: $DarkBg;
+        color: $White;
+        padding: 1rem;
+    }
+}
+
+</style>
